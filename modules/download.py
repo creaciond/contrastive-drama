4d9a11571ae5c08c0e2983deab862c4596567c49
Daria Maximova <daria.maximova.m@gmail.com>
2021-05-06 17:11:48 +0300

    relations

--- modules/download.py
+++ modules/download.py
@@ -1,4 +1,6 @@
 import requests
+import time
+from bs4 import BeautifulSoup
 
 
 class CorpusLoader:
@@ -26,7 +28,7 @@ class CorpusLoader:
         if response:
             all_plays = response.json()["dramas"]
             for play in all_plays:
-                play_ids.append(play["id"])
+                play_ids.append(play["name"])
         return play_ids
 
     def get_play_relations(self, play_id):
@@ -38,11 +40,38 @@ class CorpusLoader:
         Returns:
             relations_data: (list of lists) preprocessed relations
         """
-        request_link = f"https://dracor.org/api/corpora/{self.corpus_id}/play/{play_id}/networkdata/csv"
+        request_link = f"https://dracor.org/api/corpora/{self.corpus_id}/play/{play_id}/relations/gexf"
         response = requests.get(request_link)
-        relations_data = [line.strip("\n").split(",")
-                          for line in response.text.split("\n")]
-        return relations_data
+        relations_data = []
+        characters_info = {}
+        relations_xml = BeautifulSoup(response.text, "lxml")
+        for node in relations_xml.find_all("node"):
+            char_id = node["id"]
+            char_name = node["label"]
+            gender = "unknown"
+            for elem in node.find_all("attvalue"):
+                if "gender" in elem.attrs["for"]:
+                    gender = elem.attrs["value"]
+            characters_info[char_id] = {
+                "name": char_name,
+                "gender": gender,
+                "relations": set()
+            }
+        for relation in relations_xml.find_all("edge"):
+            if relation["type"] == "undirected" or relation["label"] in {"associated_with", "lover_of",
+                                                                         "related_with"}:
+                relations_data.append([relation["source"], relation["label"]])
+                relations_data.append([relation["target"], relation["label"]])
+            else:
+                if relation["label"] == "parent_of":
+                    relations_data.append([relation["source"], "parent"])
+                    relations_data.append([relation["target"], "child"])
+                else:
+                    print(relation["label"])
+        for relation_info in relations_data:
+            characters_info[relation_info[0]]["relations"].add(relation_info[1])
+        time.sleep(10)
+        return characters_info
 
     def get_play_text_by_character(self, play_id):
         """Downloads play lines, grouped by character.
